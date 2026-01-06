ea64d86fab51f337f9c099237e206d3849660352
vvzvvv <tmdgusv@gmail.com>
2026-01-06 14:24:28 +0900

    feat: 문제 제출 엔드포인트 뼈대 추가 - POST /problems/:problemId/submit 기본 핸들러 구현 - problemId 변수가 미사용되어서 린트에러 발생하여, 우선 eslint-disable 주석 추가함

--- apps/server/src/problems/problems.controller.ts
+++ apps/server/src/problems/problems.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Param } from '@nestjs/common';
 
 @Controller('problems')
-export class ProblemsController {}
+export class ProblemsController {
+  @Post(':problemId/submit')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  submit(@Param('problemId') problemId: string) {
+    return {
+      result: 'PASS',
+      feedback: [],
+    };
+  }
+}
