a77d10482df948d0cf794ee41b9274c8b8c62e03
caffesale <pglibrary80@gmail.com>
2026-01-08 17:13:53 +0900

    chore: 미리보기용 페이지 삭제. s3sample, cloudfront sample 페이지 삭제

--- apps/web/src/app/cloudfrontsample/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import CloudFrontStepsContainer from '@/components/cloudfront/cloudfront-steps-container'
-
-export default function SamplePage() {
-  return <CloudFrontStepsContainer />
-}
--- apps/web/src/app/s3sample/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import S3StepsContainer from '@/components/s3/s3-steps-container'
-
-export default function SamplePage() {
-  return <S3StepsContainer />
-}
