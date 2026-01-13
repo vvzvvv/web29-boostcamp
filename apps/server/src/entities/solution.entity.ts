07febbb35e28c0b29e10b703071eaf5502680a05
vvzvvv <tmdgusv@gmail.com>
2026-01-13 13:42:44 +0900

    refactor: solution 엔티티 수정 - config_type, config_info 필드 제거 - answer_config 필드 추가

--- apps/server/src/entities/solution.entity.ts
+++ apps/server/src/entities/solution.entity.ts
@@ -12,13 +12,10 @@ export class Solution {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 255 })
-  config_type: string;
-
-  @Column({ type: 'json' })
-  config_info: object;
-
   @OneToOne(() => Problem, (problem) => problem.solution)
   @JoinColumn({ name: 'problem_id' })
   problem: Problem;
+
+  @Column({ type: 'json' })
+  answer_config: Record<string, any>;
 }
