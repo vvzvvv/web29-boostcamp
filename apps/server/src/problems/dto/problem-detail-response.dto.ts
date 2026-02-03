13e5742d736f1f96aa8adf692faae110c1cc80a6
vvzvvv <tmdgusv@gmail.com>
2026-02-03 16:13:00 +0900

    refactor: ProblemDetailResponseDto의 descDetail 타입 변경 - Problem 엔티티의 descDetail 필드 타입 변경(string -> json)에 맞춰 DTO 타입을 업데이트함

--- apps/server/src/problems/dto/problem-detail-response.dto.ts
+++ apps/server/src/problems/dto/problem-detail-response.dto.ts
@@ -1,12 +1,13 @@
 import { ProblemType } from '../types/problem-type.enum';
 import { TServiceConfigMap } from '../../constants/service-convention';
+import { ProblemDescDetail } from '../types/problem-desc-detail-types';
 
 export class ProblemDetailResponseDto {
   id: number;
   problemType: ProblemType;
   title: string;
   description: string;
-  descDetail: string;
+  descDetail: ProblemDescDetail;
   requiredFields: TServiceConfigMap[];
   fixedOptions?: Record<string, any>;
   tags: string[];
