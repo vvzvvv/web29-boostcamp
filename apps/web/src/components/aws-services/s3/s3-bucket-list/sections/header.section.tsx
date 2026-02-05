741f66570569c94262eea6edb2b2a279c04450f8
caffesale <pglibrary80@gmail.com>
2026-02-05 14:51:14 +0900

    chore: remove unused button

--- apps/web/src/components/aws-services/s3/s3-bucket-list/sections/header.section.tsx
+++ apps/web/src/components/aws-services/s3/s3-bucket-list/sections/header.section.tsx
@@ -1,14 +1,6 @@
-import { Button } from '@/components/ui/button'
 import type { S3ListSectionProps } from '@/types/aws-services/s3/bucket-list'
 
-interface HeaderSectionProps extends S3ListSectionProps {
-  onCreateBucket: () => void
-}
-
-export const HeaderSection = ({
-  config,
-  onCreateBucket,
-}: HeaderSectionProps) => {
+export const HeaderSection = ({ config }: S3ListSectionProps) => {
   if (!config.header) return null
 
   return (
@@ -19,7 +11,6 @@ export const HeaderSection = ({
           Amazon S3에 저장된 객체의 컨테이너입니다.
         </p>
       </div>
-      <Button onClick={onCreateBucket}>버킷 생성</Button>
     </div>
   )
 }
